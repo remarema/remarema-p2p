d2fb5ac88f3a3ad029eca941ac57dc71f09ef91f
rebeccavl1 <rebeccavanlangelaan@yahoo.com>
2015-07-23 16:10:23 +0200

    synchronisation funktioniert. lastModified wird nach synchronisation gesetzt.

--- remarema-p2p/src/at/arz/remarema/rest/RestRepository.java
+++ remarema-p2p/src/at/arz/remarema/rest/RestRepository.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,8 +18,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 
-import remarema.p2p.FileInfo;
-import remarema.p2p.FileRepository;
+import remarema.client.filerepository.FileInfo;
+import remarema.client.filerepository.FileRepository;
 
 
 /**
@@ -26,12 +28,26 @@ import remarema.p2p.FileRepository;
  * @author Rebecca van Langelaan
  * 
  */
-public class Repository {
+public class RestRepository {
 
-	public String DIRECTORY = "\\\\A8600681\\!homelw\\rpci343\\Documents";
+	private static final Logger log = Logger.getLogger("RestRepository");
+	public String DIRECTORY = "C:\\Users\\rpci343\\server_testordner";
 	File file = new File(DIRECTORY);
 	private FileRepository repository = new FileRepository(file);
+	private String hostName;
 
+	public RestRepository() {
+
+	}
+
+	public RestRepository(String hostName) {
+		this.hostName = hostName;
+
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
 
 	/**
 	 * Listet alle Files und Unterverzeichnisse auf.
@@ -40,53 +56,45 @@ public class Repository {
 	 */
 	@GET
 	@Produces("application/xml")
-	@Path("/filelist")
+	@Path("/")
 	public Response listFiles() {
 		return listFilesFromSubdirectory("/");
 	}
 
 	@GET
-	@Path("/filelist/{subdirectory:.*}")
+	@Path("/{subdirectory:.*}")
 	public Response listFilesFromSubdirectory(@PathParam("subdirectory") String subdirectory) {
 		final File currentFile = repository.makeFileFromPath(subdirectory);
 		if (currentFile.isDirectory()) {
-			List<FileInfo> x = repository.listFiles(subdirectory);
-			GenericEntity<List<FileInfo>> entity = new GenericEntity<List<FileInfo>>(x) {
-				//
-			};
-			return Response.ok(entity, MediaType.APPLICATION_XML).build();
+			return buildDirectoryResponse(subdirectory);
 		}
 		// ok, der benutzer hat eine Datei eingegeben, oder er hat sich vertippt und die Datei existiert gar nicht.
 		if (currentFile.exists()) {
-			StreamingOutput output = new StreamingOutput() {
-
-				@Override
-				public void write(OutputStream outputStream) throws IOException {
-					FileInputStream inputStream = null;
-					try {
-						inputStream = createInputStream(currentFile);
-						byte[] buffer = new byte[1024];
-						int rc = inputStream.read();
-						while (rc != -1) {
-							outputStream.write(buffer, 0, rc);
-							rc = inputStream.read(buffer);
-						}
-					} catch (FileNotFoundException e) {
-						System.out.println("File " + currentFile.getAbsolutePath()
-											+ " could not be found on filesystem");
-					} catch (IOException e) {
-						System.out.println("Exception while reading the file" + e);
-					} finally {
-						closeInputStream(inputStream);
-					}
-				}
-			};
-			// existiert die Datei, wird mit "ok" der Status-Code 200 zurückgegeben.
-			return Response.ok(output, MediaType.APPLICATION_OCTET_STREAM).build();
+			return buildStreamResponse(currentFile);
 		}
 		// Existiert die angebebene Datei nicht, wird der Status-Code 404 für "Not Found" zurückgegeben.
-		System.out.println("not found:" + currentFile);
+		log.warning("not found:" + currentFile);
 		return Response.status(404).entity(subdirectory).build();
+	}
+
+	private Response buildStreamResponse(final File currentFile) {
+		StreamingOutput output = new StreamingOutput() {
+
+			@Override
+			public void write(OutputStream outputStream) throws IOException {
+				copyFileToDestination(currentFile, outputStream);
+			}
+		};
+		// existiert die Datei, wird mit "ok" der Status-Code 200 zurückgegeben.
+		return Response.ok(output, MediaType.APPLICATION_OCTET_STREAM).build();
+	}
+
+	private Response buildDirectoryResponse(String subdirectory) {
+		List<FileInfo> x = repository.listFiles(subdirectory);
+		GenericEntity<List<FileInfo>> entity = new GenericEntity<List<FileInfo>>(x) {
+			//
+		};
+		return Response.ok(entity, MediaType.APPLICATION_XML).build();
 	}
 
 	/**
@@ -112,6 +120,26 @@ public class Repository {
 			String msg = "InputStream konnte nicht geschlossen werden!";
 			throw new RuntimeException(msg, e);
 		}
-
 	}
+
+	void copyFileToDestination(File inputFile, OutputStream destination) throws IOException {
+		FileInputStream inputStream = createInputStream(inputFile);
+		try {
+			byte[] buffer = new byte[1024];
+
+			int len = inputStream.read(buffer);
+			while (len != -1) {
+				destination.write(buffer, 0, len);
+				len = inputStream.read(buffer);
+			}
+			destination.flush();
+		} catch (IOException e) {
+			String message = "Beim Kopieren der Datei ist ein Fehler aufgetreten";
+			log.log(Level.SEVERE, message);
+			throw new IOException(message, e);
+		} finally {
+			closeInputStream(inputStream);
+		}
+	}
+
 }
--- remarema-p2p/src/at/arz/remarema/rest/RootResource.java
+++ remarema-p2p/src/at/arz/remarema/rest/RootResource.java
@@ -2,19 +2,12 @@ package at.arz.remarema.rest;
 
 import javax.ws.rs.Path;
 
-import remarema.p2p.Hello;
-
 
 public class RootResource {
 
-	@Path("/hello")
-	public Hello helloWorld() {
-		return new Hello();
-	}
-
 	@Path("/repository")
-	public Repository repository() {
-		return new Repository();
+	public RestRepository restRepository() {
+		return new RestRepository();
 	}
 
 }
